315879c248e1cae75c4bd2f2ac774f00e06f3b04
Amit Jain <amitasn@gmail.com>
2021-06-18 22:35:19 +0530

    updated styles.css

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var txtInput = document.querySelector("#txt-input");
-var txtOutput = document.querySelector("#txt-output");
+var txtOutput = document.querySelector("#output");
 var  btnTranslate = document.querySelector("#translate");
-btnTranslate.addEventListener("click", function clickHandler() {
-  
-    txtOutput.innerText = "ABC" + txtInput.value;
-    //console.log("clicked",txtInput.value);
-})
+btnTranslate.addEventListener("click", clickHandler);
+
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+function getTranslationText(input) {
+    return serverURL + "?" + "text=" + input;
+}
+function clickHandler() {
+    
+    var inputTxt = txtInput.value;
+    fetch(getTranslationText(inputTxt))
+    .then(response => response.json())
+    .then(json =>{
+        var text = json.contents.translated
+        txtOutput.innerText = text;
+    })
+}
